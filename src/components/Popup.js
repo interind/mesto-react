3d6a17a486fe4ae497b24420081a7e0e1a392268
Aleksander <interind@yandex.ru>
2021-05-27 13:35:27 +0300

    Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,10 +4,15 @@ import classes from 'classnames';
 
 function Popup({ name, isOpen, onClose, children }) {
   const popup = classes(`popup popup_type_${name}`, { popup_opened: isOpen });
+
+  function clickClosePopup(evt) {
+    evt.currentTarget === evt.target && onClose();
+  }
+
   return (
      <div
         className={popup}
-        onMouseDown={(evt) => evt.currentTarget === evt.target && onClose()}>
+        onMouseDown={clickClosePopup}>
        {children}
      </div>
   );
